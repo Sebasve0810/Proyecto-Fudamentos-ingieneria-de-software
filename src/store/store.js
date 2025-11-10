b9770321a418c8338561360ae530ccf0e9b8c056
AndresContreras1 <conbandfel@gmail.com>
2025-11-09 21:13:01 -0500

    Add loansReducer to the Redux store

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice.js";
 import uiReducer from "./uiSlice.js";
+import loansReducer from "./loansSlice.js"; // <- nuevo
 
 export default configureStore({
   reducer: {
     auth: authReducer,
     ui: uiReducer,
+    loans: loansReducer, // <- registrar
   },
 });
