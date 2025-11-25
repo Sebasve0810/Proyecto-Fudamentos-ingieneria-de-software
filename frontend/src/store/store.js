3acb5231c74e14cb8984e6e293f2e4c5a7909d00
AndresPabon24 <andres.pabonr@javeriana.edu.co>
2025-11-25 00:31:01 -0500

    Update store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,30 +1,8 @@
-// src/store/authStore.js
-import { create } from "zustand";
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../auth/authSlice";
 
-export const useAuthStore = create((set) => ({
-  user: null,
-  token: null,
-  loading: true,
-
-  login: (user, token) => {
-    localStorage.setItem("user", JSON.stringify(user));
-    localStorage.setItem("token", token);
-    set({ user, token, loading: false });
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
   },
-
-  logout: () => {
-    localStorage.removeItem("user");
-    localStorage.removeItem("token");
-    set({ user: null, token: null, loading: false });
-  },
-
-  loadFromStorage: () => {
-    const token = localStorage.getItem("token");
-    const user = JSON.parse(localStorage.getItem("user") || "null");
-    if (token && user) {
-      set({ user, token, loading: false });
-    } else {
-      set({ loading: false });
-    }
-  },
-}));
+});
